e404f1186f84f7dae585f64ee41198baace8e379
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-03-27 16:22:31 -0300

    incluindo FichaAdmin

--- backend/capacita/admin.py
+++ backend/capacita/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Ficha
 
-admin.site.register(Ficha)
+class FichaAdmin(admin.ModelAdmin):
+    search_fields = ['nome_completo', 'cpf']
+    list_display = ('nome_completo', 'cpf')
+
+admin.site.register(Ficha, FichaAdmin)
