6fec18c2346f37b94c567607cceeb7b2c46de54b
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-04-01 12:58:12 -0300

    corrigi ficha.docx

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -157,9 +157,8 @@ STATICFILES_DIRS = [
 ]
 
 # Configurar o local onde os arquivos estáticos serão coletados para produção
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
