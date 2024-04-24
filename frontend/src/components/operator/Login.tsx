c1b64b988226e15a3d632ad0182f9db6255bd469
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-04-24 19:57:48 -0300

    pagina de login com bom tamanho

--- frontend/src/components/operator/Login.tsx
+++ frontend/src/components/operator/Login.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { Form, Input, Button } from 'antd';
+import './css/Login.css'
 
 function Login() {
   return (
-    <Form>
+    <Form className="form-login">
       <Form.Item
+        className="username-login"
         name="username"
         rules={[{ required: true, message: 'Insira seu nome de usuário!' }]}
       >
         <Input placeholder="Nome de usuário" />
       </Form.Item>
       <Form.Item
+        className="password-login"
         name="password"
         rules={[{ required: true, message: 'Insira sua senha!' }]}
       >
         <Input.Password placeholder="Senha" />
       </Form.Item>
-      <Form.Item>
+      <Form.Item className="button-login">
         <Button type="primary" htmlType="submit">
           Entrar
         </Button>
