129c9218602016a17c10441455a0e9925ad7fb83
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-05-26 19:07:33 -0300

    retirei a forma de mudar o time-zone e a linguagem pelo .env, alterei o diretorio para o banco de dados

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'pt-br'
 
 # TIME_ZONE = 'America/Fortaleza'
-TIME_ZONE = os.getenv('TIME_ZONE', 'America/Fortaleza')
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
