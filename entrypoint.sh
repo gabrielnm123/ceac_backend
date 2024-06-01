0536e8934d91ee28fdd0a72bb28f1bbe29873cc9
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-06-01 19:15:52 -0300

    copiando ./static pra /static

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py createsuperuser --noinput
+cp /app/static /static
 gunicorn core.wsgi:application --bind 0.0.0.0:8000
