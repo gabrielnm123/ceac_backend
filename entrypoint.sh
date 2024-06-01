dced25e50d597eeb4f12a59c9bc3a4b33c39f7a7
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-06-01 19:06:47 -0300

    error com arquivos estaticos resolvido

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py createsuperuser --noinput
-cp staticfiles /staticfiles
 gunicorn core.wsgi:application --bind 0.0.0.0:8000
