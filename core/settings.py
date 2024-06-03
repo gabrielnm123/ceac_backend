3adda19b943e4a4950deee62ae342a673268bbd6
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-06-02 22:14:30 -0300

    retirei comentario de codigo

--- core/settings.py
+++ core/settings.py
@@ -152,10 +152,6 @@ USE_TZ = True
 
 STATIC_URL = './static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-
 # Configurar o local onde os arquivos estáticos serão coletados para produção
 STATIC_ROOT = os.path.join(BASE_DIR, './static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
