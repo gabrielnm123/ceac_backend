1ce373ef4ef27aec1dbe113cb609f879e7a4db73
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-06-10 20:48:56 -0300

    era somente mudar pra STATIC_URL = /static/

--- core/settings.py
+++ core/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = './static/'
+STATIC_URL = '/static/'
 
 # Configurar o local onde os arquivos estáticos serão coletados para produção
 STATIC_ROOT = os.path.join(BASE_DIR, './static')
