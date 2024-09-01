18cd8731cb5b4c62afe6d6b77a4ff713cb4e1abf
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-09-01 14:45:17 -0300

    password pode ser mudado pela api

--- core/serializers.py
+++ core/serializers.py
@@ -3,9 +3,30 @@ from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = User
-        fields = ('id', 'url', 'last_login', 'is_superuser', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined', 'groups', 'user_permissions')
+        fields = ('id', 'url', 'last_login', 'is_superuser', 'username', 
+                  'first_name', 'last_name', 'email', 'is_staff', 
+                  'is_active', 'date_joined', 'groups', 'user_permissions', 
+                  'password')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = super(UserSerializer, self).create(validated_data)
+        if password is not None:
+            instance.set_password(password)
+            instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        instance = super(UserSerializer, self).update(instance, validated_data)
+        if password is not None:
+            instance.set_password(password)
+            instance.save()
+        return instance
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
