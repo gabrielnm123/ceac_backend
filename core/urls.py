1f23c1d1bee986deff8718f5c191ef881d771575
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-10-08 21:55:44 -0300

    adicionei view pra invalidar token refresh

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,6 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenVerifyView, TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView, TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 from .views import UserViewSet, GroupViewSet, PermissionViewSet, ContentTypeViewSet, get_current_user, check_password
 from capacita.views import FichaViewSet, ModulosCapacitaViewSet, download_ficha_view
 from rest_framework import routers
@@ -31,13 +15,14 @@ router.register(r'capacita/fichas', FichaViewSet)
 router.register(r'capacita/modulos_capacita', ModulosCapacitaViewSet)
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/admin')),
+    path('', RedirectView.as_view(url='/admin/')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/current_user/', get_current_user, name='current_user'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/invalidate/', TokenBlacklistView.as_view(), name='invalidate_token'),
     path('api/users/<int:user_id>/check-password/', check_password, name='check_password'),
     path('api/capacita/fichas/<int:ficha_id>/download/', download_ficha_view, name='download_ficha'),
 ]
